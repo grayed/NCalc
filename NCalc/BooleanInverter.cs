fb3da958176f17dc85f4a9c97c1b4a97ddeff8bb
Vadim Zhukov <persgray@gmail.com>
2025-01-15 08:06:08 +0300

    unbreak build

--- NCalc/BooleanInverter.cs
+++ NCalc/BooleanInverter.cs
@@ -18,6 +18,7 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
 // THE USE OR OTHER DEALINGS IN THE SOFTWARE.using System.Globalization;
 
+using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
